ff2b6e6ac27299dfbae1e37915903471b0219a83
Santiago Alfonso Rodriguez <santiago_alfonsoro@soy.sena.edu.co>
2025-06-15 14:23:06 -0500

    feat: completar modelo Book con todos los atributos, métodos y toString

--- src/com/sena/app/models/Book.java
+++ src/com/sena/app/models/Book.java
@@ -11,6 +11,9 @@ public class Book {
     private String edititionDate;
     private String editorial;
     private String isbn;
+    private List<String> authors;
+    private boolean readed;
+    private int timeReaded;
 
     // Constructor básico
     public Book(String title, String edititionDate, String editorial, String isbn) {
@@ -19,8 +22,90 @@ public class Book {
         this.edititionDate = edititionDate;
         this.editorial = editorial;
         this.isbn = isbn;
+        this.authors = new ArrayList<>();
+        this.readed = false;
+        this.timeReaded = 0;
     }
 
-    // Getters y Setters básicos
-    // TODO: Implementar getters y setters
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEdititionDate() {
+        return edititionDate;
+    }
+
+    public void setEdititionDate(String edititionDate) {
+        this.edititionDate = edititionDate;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public boolean isReaded() {
+        return readed;
+    }
+
+    public void setReaded(boolean readed) {
+        this.readed = readed;
+    }
+
+    public int getTimeReaded() {
+        return timeReaded;
+    }
+
+    public void setTimeReaded(int timeReaded) {
+        this.timeReaded = timeReaded;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder authorsStr = new StringBuilder();
+        for (int i = 0; i < authors.size(); i++) {
+            authorsStr.append(authors.get(i));
+            if (i < authors.size() - 1) {
+                authorsStr.append(", ");
+            }
+        }
+
+        return "📖 Libro ID: " + id + "\n" +
+               "   Título: " + title + "\n" +
+               "   Fecha de Edición: " + edititionDate + "\n" +
+               "   Editorial: " + editorial + "\n" +
+               "   ISBN: " + isbn + "\n" +
+               "   Autores: " + authorsStr.toString() + "\n" +
+               "   Leído: " + (readed ? "Sí" : "No") + "\n" +
+               "   Tiempo de Lectura: " + timeReaded + " horas\n" +
+               "═══════════════════════════════════════";
+    }
 }
